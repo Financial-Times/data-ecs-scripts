26197eca54e85f6dfba7787e88cb9569fa67a7c9
Margineanu, Sebastian <Sebastian.Margineanu@ft.com>
2017-12-01 13:21:55 +0000

    fix mistakes in script

--- push.sh
+++ push.sh
@@ -18,7 +18,7 @@ test -z ${ARGS[--service_name]} && ARGS[--service_name]=${1:-${SERVICE_NAME}}
 test -z ${ARGS[--image_version]} && ARGS[--image_version]=${2:-${CIRCLE_BUILD_NUM}}
 test -z ${ARGS[--aws_account_id]} && ARGS[--aws_account_id]=${3:-${AWS_DEV_ACCOUNT_NUMBER}}
 test -z ${ARGS[--aws_region]} && ARGS[--aws_region]=${4:-"eu-west-1"}
-test -z ${ARGS[--ecr_endpoint]} && ARGS[--ecr_endpoint]=${5:-${AWS_DEV_ACCOUNT_NUMBER}.ecr.eu-west-1.amazonaws.com}}
+test -z ${ARGS[--ecr_endpoint]} && ARGS[--ecr_endpoint]=${5:-${AWS_DEV_ACCOUNT_NUMBER}.dkr.ecr.eu-west-1.amazonaws.com}
 
 install_aws_cli() {
   pip install --upgrade pip
@@ -39,9 +39,9 @@ aws ecr describe-repositories --repository-names ${ARGS[--image_name]}} &>/dev/n
 #aws ecr create-repository --repository-name ${ARGS[--image_name]}
 
 echo "Tag image"
-docker tag ${ARGS[--service_name]}:${IMAGE_VERSION} \
+docker tag ${ARGS[--service_name]}:${ARGS[--image_version]} \
   ${ARGS[--ecr_endpoint]}/${ARGS[--service_name]}:${ARGS[--image_version]}
 
 
 echo "Pushing container to ECR"
-docker push ${ARGS[--ecr_endpoint]}/${ARGS[--image_name]}:${IMAGE_VERSION}
+docker push ${ARGS[--ecr_endpoint]}/${ARGS[--service_name]}:${ARGS[--image_version]}
