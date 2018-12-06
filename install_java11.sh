9bde87c3fea4c1a0160e17465d97b7c2a498cae5
Andrea Medeghini <andrea.medeghini@ft.com>
2018-12-06 12:01:09 +0000

    add script to install java 11

---
+++ install_java11.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo "Installing Java 10..."
+
+sudo wget https://download.java.net/java/GA/jdk11/13/GPL/openjdk-11.0.1_linux-x64_bin.tar.gz
+sudo tar xzvf openjdk-11.0.1_linux-x64_bin.tar.gz
+sudo mv jdk-11.0.1 /usr/lib/jvm/java-11-openjdk-amd64/
+sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/java-11-openjdk-amd64/bin/java 1
+sudo update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/java-11-openjdk-amd64/bin/javac 1
+echo 1 | sudo update-alternatives --config java
+echo 1 | sudo update-alternatives --config javac
+sudo java --version
+sudo javac --version
+
+echo "done."
